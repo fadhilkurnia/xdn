ff39cb7ef9cefbf4761fc33f943e3ba9f9faea99
ThePlatypus-Person <Ornithorhynchus0920@protonmail.com>
2025-11-27 15:04:44 +0700

    Allow runCommand to read from List<String>

--- src/edu/umass/cs/xdn/utils/Shell.java
+++ src/edu/umass/cs/xdn/utils/Shell.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.Map;
 
 public class Shell {
@@ -65,6 +66,67 @@ public class Shell {
 
     public static int runCommand(String command) {
 	return runCommand(command, true);
+    }
+
+    public static int runCommand(List<String> commandParts, boolean isSilent,
+	Map<String, String> environmentVariables) {
+	try {
+	    ProcessBuilder pb = new ProcessBuilder(commandParts);
+
+	    if (isSilent) {
+		pb.redirectOutput(ProcessBuilder.Redirect.DISCARD);
+		pb.redirectError(ProcessBuilder.Redirect.DISCARD);
+	    }
+
+	    if (environmentVariables != null) {
+		Map<String, String> processEnv = pb.environment();
+		processEnv.putAll(environmentVariables);
+	    }
+
+	    if (!isSilent) {
+		System.out.println("command: " + String.join(" ", commandParts));
+		if (environmentVariables != null) {
+		    System.out.println(environmentVariables.toString());
+		}
+	    }
+
+	    Process process = pb.start();
+
+	    if (!isSilent) {
+		try (InputStream inputStream = process.getInputStream()) {
+		    String output = new String(inputStream.readAllBytes(), StandardCharsets.ISO_8859_1);
+		    if (!output.isEmpty()) {
+			System.out.println("output:\n" + output);
+		    }
+		}
+
+		try (InputStream errStream = process.getErrorStream()) {
+		    String err = new String(errStream.readAllBytes(), StandardCharsets.ISO_8859_1);
+		    if (!err.isEmpty()) {
+			System.out.println("error:\n" + err);
+		    }
+		}
+	    }
+
+	    int exitCode = process.waitFor();
+
+	    if (!isSilent) {
+		System.out.println("exit code: " + exitCode);
+	    }
+
+	    return exitCode;
+
+	} catch (IOException | InterruptedException e) {
+	    throw new RuntimeException(e);
+	}
+    }
+
+    public static int runCommand(List<String> commandParts, boolean isSilent) {
+	return runCommand(commandParts, isSilent, null);
+    }
+
+    public static int runCommand(List<String> commandParts) {
+	return runCommand(commandParts, true);
     }
 
     public static ShellOutput runCommandWithOutput(String command, boolean isSilent,
